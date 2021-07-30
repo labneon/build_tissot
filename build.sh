2620651bd1a172d0304be2fd7583678076f56155
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-07-30 22:48:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Build script"
 #export USE_CCACHE=1
 
 # Cook
-. build/envsetup.sh
+source build/envsetup.sh
 export CCACHE_DIR=/drone/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
