b27e01ad8c7b88c0c733bfd08672a04e53f90edc
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-08-15 19:15:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo "Build script"
 export CCACHE_DIR=/drone/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-lunch ssos_rosy-userdebug
+lunch aosp_rosy-userdebug
 ccache -M 20G
 ccache -o compression=true
 ccache -z
