8ccf96e715ababf9b7ff80bffeb00e30c779f1cd
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-08-15 22:17:20 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 
 # local manifest
-git clone https://github.com/labneon/local_manifest.git --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/neonlab/local_manifest.git --depth 1 -b xr .repo/local_manifests
 
 # Sync source
 repo sync -j16 --prune -c --no-clone-bundle --optimized-fetch --no-tags --current-branch || repo sync -j16 --prune -c --no-clone-bundle --optimized-fetch --no-tags --current-branch
