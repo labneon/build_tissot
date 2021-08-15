26fa5f9ddac38ea8106caf97cb73a20c9965c783
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-08-15 19:15:33 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
 
 # local manifest
-git clone https://github.com/labneon/local_manifest.git --depth 1 -b ssos .repo/local_manifests
+git clone https://github.com/labneon/local_manifest.git --depth 1 -b aex .repo/local_manifests
 
 # Sync source
 repo sync -j16 --prune -c --no-clone-bundle --optimized-fetch --no-tags --current-branch || repo sync -j16 --prune -c --no-clone-bundle --optimized-fetch --no-tags --current-branch
