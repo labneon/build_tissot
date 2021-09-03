aa952d66a2c4d8ba440a24e0330b82e42293082b
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-09-03 20:59:16 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -6,6 +6,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.
 git clone https://github.com/labneon/local_manifest.git --depth 1 -b main .repo/local_manifests
 
 # Sync source
-repo sync -j32 --prune -c --force-sync --no-clone-bundle --no-tags || repo sync -j32 --force-sync --prune -c --no-clone-bundle --no-tags
+repo sync -j48 --prune -c --force-sync --no-clone-bundle --no-tags || repo sync -j48 --force-sync --prune -c --no-clone-bundle --no-tags
 
 echo "Sync Completed"
