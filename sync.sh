1c498a650aaa0fe2e659b6d6970f606d787025f1
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-09-06 14:27:57 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 # Initialize Repos
+tmate
 echo "Initializing Manifest"
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 
