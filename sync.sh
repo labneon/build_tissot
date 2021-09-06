9c429e6791c9b023dce8dc32886f5cec307284ed
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-09-06 12:05:38 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,12 @@
 # Initialize Repos
-echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+#echo "Initializing Manifest"
+#repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 
 # local manifest
-git clone https://github.com/labneon/local_manifest.git --depth 1 -b main .repo/local_manifests
+#git clone https://github.com/labneon/local_manifest.git --depth 1 -b main .repo/local_manifests
 
 # Sync source
-repo sync -j48 --prune -c --force-sync --no-clone-bundle --no-tags || repo sync -j48 --force-sync --prune -c --no-clone-bundle --no-tags
+#repo sync -j48 --prune -c --force-sync --no-clone-bundle --no-tags || repo sync -j48 --force-sync --prune -c --no-clone-bundle --no-tags
 
-echo "Sync Completed"
+#echo "Sync Completed"
+tmate
