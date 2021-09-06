a1a9cab89da2fad22eb37bc06bf9c6a642d4881f
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-09-06 14:20:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 #echo "Build script"
 
 # Cook
-#. build/envsetup.sh
+. build/envsetup.sh
 #export CCACHE_DIR=/drone/ccache
 #export CCACHE_EXEC=$(which ccache)
 #export USE_CCACHE=1
-#lunch lineage_tissot-userdebug
-#ccache -M 20G
-#ccache -o compression=true
-#ccache -z
-#echo "Starting Cooking"
-#make bootimage -j48
+lunch lineage_tissot-userdebug
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+echo "Starting Cooking"
+make bootimage -j64
 #make bacon -j24
 #mka vendorimage
 #ccache -s
