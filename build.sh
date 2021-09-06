2e2df0f314f63a7e756489970ea1a70740b61a4b
Lab-Neon <80776639+labneon@users.noreply.github.com>
2021-09-06 12:04:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-echo "Build script"
+#echo "Build script"
 
 # Cook
-. build/envsetup.sh
-export CCACHE_DIR=/drone/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-lunch lineage_tissot-userdebug
-ccache -M 20G
-ccache -o compression=true
-ccache -z
-echo "Starting Cooking"
-make bootimage -j48
+#. build/envsetup.sh
+#export CCACHE_DIR=/drone/ccache
+#export CCACHE_EXEC=$(which ccache)
+#export USE_CCACHE=1
+#lunch lineage_tissot-userdebug
+#ccache -M 20G
+#ccache -o compression=true
+#ccache -z
+#echo "Starting Cooking"
+#make bootimage -j48
 #make bacon -j24
 #mka vendorimage
-ccache -s
+#ccache -s
 
-echo "Build Done"
-cd out/target/product/tissot
-curl --upload-file boot.img https://free.keep.sh
+#echo "Build Done"
+#cd out/target/product/tissot
+#curl --upload-file boot.img https://free.keep.sh
