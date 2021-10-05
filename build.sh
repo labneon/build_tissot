26789baa842ee7b0c8ddef5ad1062ab2159df635
neonmicky <neonmicky30@gmail.com>
2021-10-05 10:06:58 +0000

    update

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@
 #export CCACHE_DIR=/drone/ccache
 #export CCACHE_EXEC=$(which ccache)
 #export USE_CCACHE=1
-lunch lineage_tissot-userdebug
+lunch aosp_tissot-eng
 ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-make bootimage -j24
-#make bacon -j24
+#make bootimage -j24
+make bacon -j24
 #mka vendorimage
 #ccache -s
 
--- sync.sh
+++ sync.sh
@@ -1,11 +1,18 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://android.googlesource.com/platform/manifest -b android-12.0.0_r2 -g default,-device,-mips,-darwin,-notdefault
 
 # local manifest
-git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b cr .repo/local_manifests
+#git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b cr .repo/local_manifests
 
 # Sync source
 repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune -c --no-clone-bundle --no-tags
-
+git clone https://github.com/labneon/device_xiaomi_tissot -b aosp device/xiaomi/tissot --depth=1
+git clone https://github.com/labneon/device_xiaomi_msm8953-common -b aosp device/xiaomi/msm8953-common --depth=1
+git clone https://github.com/neonmicky/kernel_xiaomi_tissot -b test kernel/xiaomi/msm8953 --depth=1
+git clone https://gitlab.com/the-muppets/proprietary_vendor_xiaomi.git -b lineage-18.1 vendor/xiaomi --depth=1 --single-branch
+rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
 #echo "Sync Completed"
