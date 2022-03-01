098532f99748a1206d647875a144c3402a831128
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-03-01 10:42:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch aospa_tissot-userdebug
-./rom-build.sh tissot
+lunch arrow_tissot-userdebug
+m sepolicy
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
