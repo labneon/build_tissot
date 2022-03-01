c07ca83300badf860a7d47735e6faf430b8f3cfb
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-03-01 10:42:09 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 -u https://github.com/AOSPA/manifest -b sapphire
-git clone https://github.com/neonmicky/local_manifest.git -b sapphire .repo/local_manifests --depth=1
+repo init --depth=1 -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0
+git clone https://github.com/neonmicky/local_manifest.git -b arrow .repo/local_manifests --depth=1
 
 # Sync source
 repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune -c --no-clone-bundle --no-tags
