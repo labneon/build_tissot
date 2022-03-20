922c2aeaa4bf27edad034d376b890412913f4488
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-03-20 13:18:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #echo "Build script"
-
+rm -rf hardware/qcom/media hardware/qcom/display
+git clone https://github.com/aospneon/msm8996-Hals -b dis hardware/qcom/display --depth=1
+git clone https://github.com/aospneon/msm8996-Hals -b med hardware/qcom/media --depth=1
 # Cook
 source build/envsetup.sh
 ccache -M 20G
@@ -8,7 +10,6 @@ ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
 lunch aospa_tissot-userdebug
-make bootimage
 ./rom-build.sh tissot
 #echo "Build Done"
 #cd out/target/product/tissot
