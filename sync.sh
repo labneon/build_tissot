2573564b04cd8023c057e39421cecf8213135dc5
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-03-25 22:33:43 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,8 +9,8 @@ repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune 
 git clone https://github.com/aospneon/device_xiaomi_tissot -b lineage-19.1 device/xiaomi/tissot --depth=1
 git clone https://github.com/aospneon/device_xiaomi_msm8953-common -b lineage-19.1 device/xiaomi/msm8953-common --depth=1
 git clone https://github.com/neonmicky/kernel_xiaomi_tissot -b twelve kernel/xiaomi/msm8953 --depth=1
-git clone https://github.com/neonmicky/kernel_xiaomi_tissot -b 12.0-tissot vendor/xiaomi/tissot --depth=1
-git clone https://github.com/neonmicky/kernel_xiaomi_tissot -b 12.0-common vendor/xiaomi/msm8953-common --depth=1
+git clone https://github.com/neonmicky/vendor_xiaomi -b 12.0-tissot vendor/xiaomi/tissot --depth=1
+git clone https://github.com/neonmicky/vendor_xiaomi -b 12.0-common vendor/xiaomi/msm8953-common --depth=1
 rm -rf hardware/qcom-caf/msm8996/display hardware/qcom-caf/msm8996/media
 git clone https://github.com/aospneon/msm8996-Hals -b display hardware/qcom-caf/msm8996/display
 git clone https://github.com/aospneon/msm8996-Hals -b media hardware/qcom-caf/msm8996/media
