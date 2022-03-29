03d69bcfd8b4565519cb57a550b78d3a239959f5
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-03-29 22:25:21 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1
-git clone https://github.com/aospneon/local_manifest.git -b lineage-19.1 .repo/local_manifests --depth=1
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1
+git clone https://github.com/aospneon/local_manifest.git -b lineage-18.1 .repo/local_manifests --depth=1
 
 # Sync source
 repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune -c --no-clone-bundle --no-tags
