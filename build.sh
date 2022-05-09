ef486f2d99eef5c52254c71e335d0e60837b9421
Lab-Neon <80776639+labneon@users.noreply.github.com>
2022-05-09 17:13:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch lineage_tissot-userdebug
-m sepolicy
+lunch p404_tissot-userdebug
+make bacon
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
